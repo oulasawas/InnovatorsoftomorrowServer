5d846052d515c3bace154ef7bfa2573c637cc562
oulasawas <oula.sawas92@gmail.com>
2025-07-27 12:47:47 -0400

    changing port to 3004

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 
 dotenv.config();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3004
 const app = express();
 app.use(cors());
 app.use(express.json());
