2701bc2501c42b7ff1de4efae88360191e0f3574
oulasawas <59581710+oulasawas@users.noreply.github.com>
2025-11-22 15:11:24 -0500

    Add sprite routes to the API

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const userRoutes = require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 const openAIRoutes = require('./routes/openAIRoutes');
 const mediaRoutes = require('./routes/mediaRoutes');
+const spriteController = require('./routes/spriteRoutes');
 
 dotenv.config();
 const PORT = process.env.PORT || 4000
@@ -20,7 +21,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/teachAI', openAIRoutes);
 app.use('/api/media', mediaRoutes);
-
+app.use('/api/sprite', spriteController);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log('Server running on port '+PORT)))
   .catch(err => console.log(err));
